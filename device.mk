1d45cc475cb7348e6b9a602cca5dfd4c9cfe6f03
Demon000 <demonsingur@gmail.com>
2020-07-13 00:48:05 +0300

    gram: use dynamic partitions
    Change-Id: Iac89f608a7f8efa78da5200c68cb45ab2f439fd0

--- device.mk
+++ device.mk
@@ -80,6 +80,9 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
