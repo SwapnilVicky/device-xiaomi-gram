f455876ad37c91a7e5e40cf60bc5f053cae86828
Demon000 <demonsingur@gmail.com>
2022-08-01 07:52:03 +0700

    gram: Define board platform rules * Taken from https://source.codeaurora.org/quic/la/platform/vendor/qcom/atoll/tree/BoardConfig.mk?h=LA.UM.8.9.r1-09500-SM6xx.0#n6
    Can be cross-checked with the following prop: ro.board.platform
    
    Signed-off-by: SwapnilVicky <swapnil.motoe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,4 +22,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
+# Platform
+TARGET_BOARD_PLATFORM := atoll
+
 -include vendor/xiaomi/gram/BoardConfigVendor.mk
