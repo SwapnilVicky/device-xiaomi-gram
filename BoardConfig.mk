cdbb2312a4245ff8fa4f15b273bebebb5fe964d7
SwapnilVicky <swapnil.motoe@gmail.com>
2022-11-26 19:03:01 +0530

    gram: Define init library

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,10 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_gram
+TARGET_RECOVERY_DEVICE_MODULES := libinit_gram
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
 BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
